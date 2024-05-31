0183f175cc76c9f35844d5bcfaed82f882889338
JamesBaiJun <42957381+JamesBaiJun@users.noreply.github.com>
2024-05-31 10:25:55 +0800

    添加动画

---
+++ LoadingAnimation.Avalonia/Progress/Progress6.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace LoadingAnimation.Avalonia.Progress
+{
+    public partial class Progress6 : UserControl
+    {
+        public Progress6()
+        {
+            InitializeComponent();
+        }
+    }
+}
