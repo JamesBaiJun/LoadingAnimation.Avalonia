5f59a2a439f4ef53abc7f1bcbdaebde146bf7e53
JamesBaiJun <42957381+JamesBaiJun@users.noreply.github.com>
2024-06-04 10:00:44 +0800

    添加动画

---
+++ LoadingAnimation.Avalonia/Classic/Classic7.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace LoadingAnimation.Avalonia.Classic
+{
+    public partial class Classic7 : ClassicBase
+    {
+        public Classic7()
+        {
+            InitializeComponent();
+        }
+    }
+}
