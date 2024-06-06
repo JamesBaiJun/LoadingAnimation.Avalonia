9f812134e4151acdd08579a33a0c28f3f7410a75
JamesBaiJun <42957381+JamesBaiJun@users.noreply.github.com>
2024-06-06 17:12:45 +0800

    添加动画

---
+++ LoadingAnimation.Avalonia/Spinner/Spinner5.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LoadingAnimation.Avalonia.Spinner;
+
+public partial class Spinner5 : UserControl
+{
+    public Spinner5()
+    {
+        InitializeComponent();
+    }
+}
